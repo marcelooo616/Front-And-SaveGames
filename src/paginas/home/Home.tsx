2b9b9bcb27452a70a4675f12bab0a5c68047c3ea
marcelooo616 <marcelooo2014@hotmail.com>
2022-09-07 23:15:48 -0300

    fix - carousel

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,16 @@
+import { Grid } from "@material-ui/core";
+import { Box } from "@mui/material";
 import React from "react";
+import Carousel from "../../componentes/carousel/carousel";
 
 function Home(){
     return (
         <>
-        <div className="home">
-            <h1>Home</h1>
-        </div>
+        <Grid xs={12} >
+          <Box>
+            <Carousel />
+          </Box>
+        </Grid>
         </>
     );
 }
